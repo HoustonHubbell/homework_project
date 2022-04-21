a3941e7da8e16e8597262ed114c29b472a4b9eac
HoustonHubbell <hootie87229@gmail.com>
2022-04-20 22:40:34 -0500

    day_3_changes

--- car_inventory/__init__.py
+++ car_inventory/__init__.py
@@ -7,7 +7,13 @@ from .api.routes import api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from .models import db as root_db
+from .models import db as root_db, login_manager, ma
+
+from flask_marshmallow import Marshmallow
+
+from flask_cors import CORS
+
+from car_inventory.helpers import JSONEncoder
 
 app = Flask(__name__)
 
@@ -20,3 +26,12 @@ app.config.from_object(Config)
 root_db.init_app(app)
 
 migrate = Migrate(app, root_db)
+
+login_manager.init_app(app)
+login_manager.login_view = 'auth.signin'
+
+ma.init_app(app)
+
+app.json_encoder = JSONEncoder
+
+CORS(app)
--- car_inventory/models.py
+++ car_inventory/models.py
@@ -8,11 +8,23 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 import secrets
 
+from flask_login import UserMixin, LoginManager
+
+
+
+
+from flask_marshmallow import Marshmallow
 
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+ma = Marshmallow()
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
     id = db.Column(db.String, primary_key = True)
     first_name = db.Column(db.String(150), nullable = True, default = '')
     last_name = db.Column(db.String(150), nullable = True, default = '')
@@ -21,6 +33,7 @@ class User(db.Model):
     g_auth_verify = db.Column(db.Boolean, default = False)
     token = db.Column(db.String, default = '', unique = True)
     date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    coin = db.relationship('Coin', backref = 'owner', lazy = True)
 
 
     def __init__(self, email, first_name = '', last_name = '', id = '', password = '', token = '', g_auth_verify = False):
@@ -46,3 +59,41 @@ class User(db.Model):
     def __repr__(self):
         return f"User {self.email} has been added to the database"
         
+
+
+class Coin(db.Model):
+    id = db.Column(db.String, primary_key = True)
+    name = db.Column(db.String(150))
+    description = db.Column(db.String(200), nullable = True)
+    price = db.Column(db.Numeric(precision=10, scale=2))
+    volume = db.Column(db.Numeric(precision=10, scale=2), nullable = True)
+    ticker = db.Column(db.String(100))
+    market_cap = db.Column(db.Numeric(precision=10, scale=2))
+    percent_change = db.Column(db.Numeric(precision=10, scale=2))
+    user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable = False)
+
+    def __init__(self, name, description, price, volume, ticker, market_cap, percent_change, user_token, id = ''):
+        self.id = self.set_id()
+        self.name = name
+        self.description = description
+        self.price = price
+        self.volume = volume
+        self.ticker = ticker
+        self.market_cap = market_cap
+        self.percent_change = percent_change
+        self.user_token = user_token
+
+    def __repr__(self):
+        return f"The following Coin has been added: {self.name}"
+
+    def set_id(self):
+        return (secrets.token_urlsafe())
+
+
+
+class CoinSchema(ma.Schema):
+    class Meta:
+        fields = ['id', 'name', 'description', 'price', 'volume', 'ticker', 'market_cap', 'percent_change']
+
+coin_schema = CoinSchema()
+coins_schema = CoinSchema(many = True)
